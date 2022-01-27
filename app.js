6903902ccefd6776f4b0ee6ba4d6d70f64f6bf19
eryanph <eryan.gamlanga@gmail.com>
2022-01-27 18:24:12 +0800

    Refactor source code for production level

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const { default: axios } = require('axios');
-const { Telegraf } = require('telegraf');
-require('dotenv').config();
+import axios from 'axios';
+import { Telegraf } from 'telegraf';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const chatId = process.env.CHAT_ID;
 const witnetExplorer = process.env.WITNET_EXPLORER;
 const whaleThreshold = process.env.WHALE_THRESHOLD;
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const explorerClient = axios.create({
+  timeout: 30000,
+  baseURL: witnetExplorer
+})
 
 const HASH = 0;
 const EPOCH = 1;
@@ -14,117 +20,159 @@ const CONFIRMATION = 10;
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+const request = (url) => new Promise(async (resolve) => {
+  explorerClient.get(url)
+    .then(res => {
+      resolve(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+      resolve(null);
+    });
+});
+
 const constructTgMessage = (hash, value) => {
   let msg = '🐳🐳 <b>WHALE ALERT</b> 🐳🐳\n\n';
-  msg += `A TXN involving <code>${value}</code> WITs has occurred.\n\n`;
+  msg += `A TXN involving <code>${value.toLocaleString()} WIT</code> has occurred.\n\n`;
   msg += `🔍 ${witnetExplorer}/search/${hash}`;
   return msg;
 }
 
-const getBlockTxns = (hash) => new Promise((resolve) => {
-  axios.get(`${witnetExplorer}/api/hash?value=${hash}`)
-    .then(res => {
-      const data = res.data;
-      resolve(data.value_transfer_txns);
-    })
-    .catch(err => {
-      console.log(err);
-      resolve(null);
-    });
-});
+const getBlockTxns = async (hash) => {
+  const data = await request(`/api/hash?value=${hash}`);
+  if (data) {
+    return data.value_transfer_txns;
+  } else {
+    return null;
+  }
+};
 
-// TODO: add timeout
-const getLastEpoch = () => new Promise((resolve) => {
-  axios.get(`${witnetExplorer}/api/blockchain?action=init&block=-1`)
-    .then(res => {
-      const data = res.data;
-      let lastEpoch = null;
-      if (data.blockchain) {
+const getLastEpoch = async () => {
+  const data = await request('/api/blockchain?action=init&block=-1');
+  if (data) {
+    let lastEpoch = null;
+    if (data.blockchain) {
+      try {
         lastEpoch = data.blockchain[0][EPOCH];
+      } catch (err) {
+        console.log(err);
       }
-      resolve(lastEpoch);
-    })
-    .catch(err => {
-      console.log(err);
-      resolve(null);
-    });
-});
+    }
+    return lastEpoch;
+  } else {
+    return null;
+  }
+};
 
-const getBlocks = (epoch) => new Promise((resolve) => {
-  axios.get(`${witnetExplorer}/api/blockchain?action=update&block=${epoch}`)
-    .then(res => {
-      const data = res.data;
-      let blocks = null;
-      if (data.blockchain) {
-        blocks = data.blockchain;
-      }
-      resolve(blocks);
-    })
-    .catch(err => {
-      console.log(err);
-      resolve(null);
-    });
-});
+const getBlocks = async (epoch) => {
+  const data = await request(`/api/blockchain?action=update&block=${epoch}`); 
+  if (data) {
+    let blocks = null;
+    if (data.blockchain) {
+      blocks = data.blockchain;
+    }
+    return blocks;
+  } else {
+    return null;
+  }
+};
 
 const getLastConfirmedEpoch = async () => {
   const lastEpoch = await getLastEpoch();
   console.log(`last epoch: ${lastEpoch}`);
-  let lastConfirmedEpoch = null;
 
-  let epochOffset = 0;
+  if (lastEpoch) {
+    let lastConfirmedEpoch = null;
+    let epochOffset = 0;
 
-  while(lastConfirmedEpoch == null) {
-    epochOffset += 10;
-    const readEpoch = lastEpoch - epochOffset;
-    const blocks = await getBlocks(readEpoch);
-    
-    for (const block of blocks) {
-      if (block[CONFIRMATION]) {
-        lastConfirmedEpoch = block[EPOCH];
-        break;
+    while (lastConfirmedEpoch == null) {
+      const readEpoch = lastEpoch - epochOffset;
+      const blocks = await getBlocks(readEpoch);
+      
+      if (blocks) {
+        for (const block of blocks) {
+          if (block[CONFIRMATION]) {
+            lastConfirmedEpoch = block[EPOCH];
+            break;
+          }
+        }
+      } else {
+        await sleep(1000);
+        continue;
       }
+      epochOffset += 10;
     }
+    
+    return lastConfirmedEpoch;
+  } else {
+    return null;
   }
-
-  return lastConfirmedEpoch;
 }
 
 const explorerScanner = async () => {
-  let lastReadEpoch = await getLastConfirmedEpoch() - 1;
-  // let lastReadEpoch = 888302;
+  let lastReadEpoch = null;
+  while (lastReadEpoch == null) {
+    lastReadEpoch = await getLastConfirmedEpoch() - 1;
 
-  while(true) {
+    if (lastReadEpoch == null) {
+      await sleep(10000);
+    }
+  }
+
+  while (true) {
     const lastEpoch = await getLastEpoch();
-    console.log(`last epoch: ${lastEpoch}\nlast read epoch: ${lastReadEpoch}`);
 
-    if (lastEpoch > lastReadEpoch) {
-      const blocks = await getBlocks(lastReadEpoch);
-      
-      console.log(`found ${blocks.length} blocks`);
-      let blockIndex = 1;
-      for (const block of blocks) {
-        console.log(`[${blockIndex++}/${blocks.length}] reading block ${block[HASH]}`);
-        if (block[CONFIRMATION]) {
-          lastReadEpoch = block[EPOCH];
-          if (block[TRANSACTION_VALUE] > 0) {
-            const txns = await getBlockTxns(block[HASH]);
-            for (const txn of txns) {
-              const hash = txn.txn_hash;
-              const value = txn.value * 1e-9;
+    if (lastEpoch) {
+      console.log(`last epoch: ${lastEpoch}\nlast read epoch: ${lastReadEpoch}`);
 
-              if (value > whaleThreshold) {
-                const msg = constructTgMessage(hash, value);
-                await bot.telegram.sendMessage(chatId, msg, {parse_mode: 'html'})
+      if (lastEpoch > lastReadEpoch) {
+        const blocks = await getBlocks(lastReadEpoch);
+        
+        if (blocks) {
+          console.log(`found ${blocks.length} blocks`);
+
+          let blockIndex = 1;
+          for (const block of blocks) {
+            console.log(`[${blockIndex++}/${blocks.length}] reading block ${block[HASH]}`);
+
+            if (block[CONFIRMATION]) {
+              lastReadEpoch = block[EPOCH];
+
+              if (block[TRANSACTION_VALUE] > 0) {
+                console.log('- found transaction value. reading txns...');
+
+                let txns = null;
+                while (txns == null) {
+                  txns = await getBlockTxns(block[HASH]);
+
+                  if (txns == null) {
+                    await sleep(10000);
+                    console.log('- retrying reading txns...');
+                  }
+                }
+
+                for (const txn of txns) {
+                  const hash = txn.txn_hash;
+                  console.log(`- reading txn ${hash}`);
+
+                  const value = txn.value * 1e-9;
+                  if (value >= whaleThreshold) {
+                    console.log('- exceeded threshold. sending alert...')
+
+                    const msg = constructTgMessage(hash, value);
+                    await bot.telegram.sendMessage(chatId, msg, {parse_mode: 'html'})
+                  }
+
+                  await sleep(5000);
+                }
               }
-
-              await sleep(5000);
             }
           }
         }
       }
     }
 
-    await sleep(1000);
+    await sleep(10000);
   }
 }
 
@@ -133,5 +181,11 @@ console.log('Telegram bot is now running...');
 explorerScanner();
 
 // Graceful exits
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+process.once('SIGINT', () => {
+  bot.stop('SIGINT');
+  process.exit(0);
+});
+process.once('SIGTERM', () => {
+  bot.stop('SIGTERM');
+  process.exit(0);
+});
